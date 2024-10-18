49f350a3222bb16546f65d36c60ec6ffb01b49a5
Serhii Kimlyk <serhii.kimlyk@wolterskluwer.com>
2024-10-18 13:02:23 +0300

    fix: base64 converter layout

--- src/app/features/tools/base64/base64converter.component.ts
+++ src/app/features/tools/base64/base64converter.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnDestroy,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject, takeUntil } from 'rxjs';
+import { BACKEND_URL } from '../../../app.constants';
 
 @Component({
-  selector: 'app-base-64-converter-component',
+  selector: 'kn-base-64-converter-component',
   templateUrl: 'base64converter.component.html',
   styleUrls: ['base64converter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
-export class Base64converterComponent {
+export class Base64converterComponent implements OnDestroy {
+  private _disposed$ = new Subject<void>();
   convertFrom: string = '';
   convertTo: string = '';
 
+  constructor(
+    @Inject(BACKEND_URL) private backendUrl: string,
+    private httpClient: HttpClient,
+  ) {}
+
+  ngOnDestroy() {
+    this._disposed$.next();
+    this._disposed$.complete();
+  }
+
   onConvertFrom64() {
-    this.convertTo = btoa(this.convertFrom);
+    this.httpClient
+      .post<{ converted: string }>(`${this.backendUrl}/api/base64/from`, {
+        inputText: this.convertTo,
+      })
+      .pipe(takeUntil(this._disposed$))
+      .subscribe(({ converted }) => (this.convertFrom = converted));
   }
 
   onConvertTo64() {
-    this.convertFrom = atob(this.convertTo);
+    this.httpClient
+      .post<{ converted: string }>(`${this.backendUrl}/api/base64/to`, {
+        inputText: this.convertFrom,
+      })
+      .pipe(takeUntil(this._disposed$))
+      .subscribe(({ converted }) => (this.convertTo = converted));
   }
 }
